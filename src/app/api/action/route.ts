0ed2fe63b928dea0e7462c18cf7755d6feaf6cef
buhama <hassan@hassanahmed.ca>
2025-05-05 01:32:55 -0400

    write to database

--- src/app/api/action/route.ts
+++ src/app/api/action/route.ts
@@ -1,6 +1,10 @@
 import { ToolInvocation, streamText } from 'ai';
 import { google } from '@ai-sdk/google';
 import { z } from 'zod';
+import { createClient } from '@/lib/supabase/server';
+import { Reminder } from '@/models/Reminder';
+import { getRandomId } from '@/lib/string';
+import { getTodaysDate } from '@/lib/date/date';
 
 interface Message {
 	role: 'user' | 'assistant';
@@ -9,33 +13,63 @@ interface Message {
 }
 
 export async function POST(req: Request) {
+	const supabase = await createClient();
 	try {
 		const { messages }: { messages: Message[] } = await req.json();
 
-		console.log('messages', messages);
-		// eslint-disable-next-line @typescript-eslint/no-explicit-any
-		console.log('parts', (messages[0] as any).parts);
+		const newAssistantMessage: Message = {
+			role: 'assistant',
+			content: [
+				'You are a virtual assistant for the user.',
+				'The user is journaling and it is your job to take their entries and take action on their behalf.',
+				'You will take the latest journal entry and take one of the actions that you are given,',
+				'or take no action if you determine that no action is needed',
+			].join(' '),
+		};
+
+		messages.unshift(newAssistantMessage);
 
 		const result = streamText({
 			model: google('gemini-2.0-flash'),
 			system: 'You are a helpful assistant.',
 			messages,
 			tools: {
-				getWeather: {
-					description: 'Get the weather for a location',
+				addReminder: {
+					description:
+						'Based on the journal entry determine if a reminder needs to be added then return the reminder. Return nothing if no reminder is to be added.',
 					parameters: z.object({
-						city: z.string().describe('The city to get the weather for'),
-						unit: z
-							.enum(['C', 'F'])
-							.describe('The unit to display the temperature in'),
+						title: z.string().describe('Title of the reminder'),
+						description: z.string().describe('Description of the reminder'),
 					}),
-					execute: async ({ city, unit }) => {
-						const weather = {
-							value: 24,
-							description: 'Sunny',
+					execute: async ({ title, description }) => {
+						const { data: userData, error: userError } =
+							await supabase.auth.getUser();
+
+						if (userError || !userData?.user) {
+							throw new Error('User not found');
+						}
+
+						const newReminder: Reminder = {
+							id: getRandomId(),
+							title,
+							description,
+							created_at: getTodaysDate(),
+							updated_at: getTodaysDate(),
+							completed_at: null,
+							user_id: userData.user.id,
 						};
 
-						return `It is currently ${weather.value}°${unit} and ${weather.description} in ${city}!`;
+						const { data: reminderData, error: reminderError } = await supabase
+							.from('reminders')
+							.insert(newReminder)
+							.select()
+							.single<Reminder>();
+
+						if (reminderError) {
+							throw new Error('Error adding reminder');
+						}
+
+						return reminderData;
 					},
 				},
 			},
